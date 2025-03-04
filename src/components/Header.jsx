e8dd0bfb14ad2dc733e53b589b3abef8a19bb2d8
sahil <maleksahil9315@gmail.com>
2025-03-04 23:52:17 +0530

    fake-api

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
+import { SidebarContext } from '../contexts/SidebarContext'
 const Header = () => {
+  const {setIsOpen} = useContext(SidebarContext);
   return (
     <div>
       <h1>Header</h1>
       <Link to="/">Home</Link>
+      <div onClick={()=> setIsOpen(!isOpen)}>open/close sidebar</div>
     </div>
   )
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { FiTrash2 } from 'react-icons/fi';
+import { IoMdArrowForward } from 'react-icons/io';
+import { Link } from 'react-router-dom';
+import CartItem from "../components/CartItem";
+import { SidebarContext } from '../contexts/SidebarContext';
 
 const Sidebar = () => {
-  return (
-    <div>
-      <h1>Sidebar</h1>
-    </div>
-  )
-}
+  const { isOpen, handleClose } = useContext(SidebarContext);
 
-export default Sidebar
+  return (
+    <div className={`${isOpen ? 'right-0' : '-right-full'} w-full bg-white fixed top-0 h-full shadow-2xl md:w-[35vw] xl:max-w-[30vw] transition-all duration-300 z-20 px-4 lg:px-[35px]`}>
+    
+      Sidebar
+      {/* Add your sidebar content here */}
+    </div>
+  );
+};
+
+export default Sidebar;
